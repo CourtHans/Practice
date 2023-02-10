65f8ac523bbc31f1bf1b88214e2384d419d75a30
CourtHans <cornta@yahoo.com>
2023-02-09 21:50:24 -0800

    playing around

---
+++ practice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Courtney Hans
+# Date of latest revision: 2/9/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "Hello World"
