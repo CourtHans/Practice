43524da1357ad499de1136e402fc73740d7d7431
CourtHans <cornta@yahoo.com>
2023-02-13 21:36:58 +0000

    practice

---
+++ bash1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Courtney Hans
+# Date of latest revision: 2/13/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "Here is my working directory:"
+pwd
+echo "This is the current user:"
+whomai
+
+
+# End
