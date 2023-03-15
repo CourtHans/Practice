94a1314bba38ff6da1b0033e767c6d684958baca
Court Hans <cornta@yahoo.com>
2023-03-14 20:08:19 -0700

    Create moreBash.sh

---
+++ moreBash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script:                   Class 03 demo
+# Author:                   Courtney Hans
+# Date of last revision:    3/14/23
+# Purpose:                  Write a script that prints the hostname of this computer.
+#                           Your script must use at least one function and one variable.
+
+# Main
+
+echo "for the purposes of demo-ing git pull"
+
+# End
